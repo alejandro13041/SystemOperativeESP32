fe6ea57d4ca26e442cac7c4b591229cf6b5a83ce
alejandro13041 <83832792+alejandro13041@users.noreply.github.com>
2023-06-09 13:11:17 -0500

    Update database.h
    best functions

--- lib/database.h
+++ lib/database.h
@@ -43,10 +43,13 @@ void space(){
     std::cout<<"          ";
 
  }
+
+ 
+ 
 void printTable(const std::vector<root>& r1){
 
     std::cout<<"+========================================================================+"<<'\n';
-    std::cout<<" id          user          pass          mail          rank"<<'\n';   
+    std::cout<<" id          user          pass          mail          rank      sizeoff"<<'\n';   
 
 
     for (int i = 0; i < (int) r1.size(); i++){
@@ -54,17 +57,21 @@ void printTable(const std::vector<root>& r1){
 		color(hConsole,14);
         std::cout<<" "<<i; space();
         color(hConsole,10);
-        std::cout<<""<<r1[i].user; space();
+        std::cout<<r1[i].user; space();
         color(hConsole,12);
-        std::cout<<""<<r1[i].pass; space();
+        std::cout<<r1[i].pass; space();
         color(hConsole,11);
-        std::cout<<""<<r1[i].mail; space();
+        std::cout<<r1[i].mail; space();
         color(hConsole,13);
-        std::cout<<""<<r1[i].rank<<'\n'; 
+        std::cout<<r1[i].rank; space();
+        color(hConsole,15);
+        std::cout<<'\n';
+        //std::cout<<sizeof(r1[i])<<"bytes"<<'\n'; 
 
      } 
      
      color(hConsole,15);
+
 
     /*
 
@@ -76,11 +83,47 @@ void printTable(const std::vector<root>& r1){
     */    
 
  }
-void add_user(){
 
+void tableMemory(const std::vector<root>& r1){
+
+    std::cout<<"+========================================================================+"<<'\n';
+    std::cout<<"                                 table memory                             "<<'\n';
+    std::cout<<"+========================================================================+"<<'\n';
+    std::cout<<" id (bytes)          user(bytes)          pass(bytes)          mail(bytes)          rank(bytes)     object(bytes)"<<'\n';   
+
+
+    for (int i = 0; i < (int) r1.size(); i++){
+
+		color(hConsole,14);
+        std::cout<<" "<<sizeof(i)<<"(bytes)"; space();
+        color(hConsole,10);
+        std::cout<<sizeof(r1[i].user)<<"(bytes)"; space();
+        color(hConsole,12);
+        std::cout<<sizeof(r1[i].pass)<<"(bytes)"; space();
+        color(hConsole,11);
+        std::cout<<sizeof(r1[i].mail)<<"(bytes)"; space();
+        color(hConsole,13);
+        std::cout<<sizeof(r1[i].rank)<<"(bytes)"; space();
+        color(hConsole,15);
+        std::cout<<sizeof(r1[i])<<"(bytes)"<<'\n'; 
+
+     } 
+     
+     color(hConsole,15);
+
+
+    /*
+
+        user: user1
+        pass: password1
+        mail: user1@mail.com
+        rank: root
+
+    */
 
 
  }
+    
 void add_user(std::string user,std::string pass, std::string mail, std::string rank){
 
     root r1;
@@ -101,6 +144,8 @@ void add_user(std::string user,std::string pass, std::string mail, std::string r
  
  void add_user_line(){
 
+    std::cout<<"[main] start";
+
 	root r1;
 
 	std::cout<<"[main] string: user:";
@@ -116,6 +161,18 @@ void add_user(std::string user,std::string pass, std::string mail, std::string r
 
 
  }
+
+void defect_database(){
+
+    add_user("user1","password1","user1@mail.com","root1");
+    add_user("user2","password2","user2@mail.com","root2");
+    add_user("user3","password3","user3@mail.com","root3");
+    add_user("user4","password4","user4@mail.com","root4");
+    add_user("user5","password5","user5@mail.com","root5");
+
+
+ }
+
 struct Wifi{
 
     std::string ssid;
